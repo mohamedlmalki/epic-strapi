2eb87ca26a6fa5fb807c60d281fee23e46b78d04
Paul Bratslavsky <paulbratslavsky@Pauls-MBP.localdomain>
2024-11-09 13:25:25 -0600

    initial commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,49 @@
+import qs from "qs";
+
+import { HeroSection } from "@/components/custom/hero-section";
+
+const homePageQuery = qs.stringify({
+  populate: {
+    blocks: {
+      on: {
+        "layout.hero-section": {
+          populate: {
+            image: {
+              fields: ["url", "alternativeText"],
+            },
+            link: {
+              populate: true,
+            },
+          },
+        },
+      },
+    },
+  },
+});
+
+async function getStrapiData(path: string) {
+  const baseUrl = "http://localhost:1337";
+
+  const url = new URL(path, baseUrl);
+  url.search = homePageQuery;
+
+  try {
+    const response = await fetch(url.href);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default async function Home() {
+  const strapiData = await getStrapiData("/api/home-page");
+
+  const { title, description, blocks } = strapiData.data;
+
+  return (
+    <main>
+      <HeroSection data={blocks[0]} />
+    </main>
+  );
+}
